9e169e665b9c139c43976e0c42627d4245f41515
Andrew Anderson <andrew.wja@gmail.com>
2009-04-04 18:52:08 +0000

    Added the wiki page on VM model to the appropriate Doc section, slight changes to sexp.c
    git-svn-id: svn+ssh://spoon/var/www/vhosts/trac/altitude/svn/trunk@9 980be3b3-3046-4121-8ed5-61669e3ce2bc

--- vm/sexp.c
+++ vm/sexp.c
@@ -16,8 +16,32 @@ const char* sexp_tag_to_string(sexp_tag t){
   return sexp_names[t];
 }
 
-/* FIXME: incomplete */
+static char* ltrim(char* str){
+  char* end = str;
+  while(*end != 0){
+    if(isspace(*end)){
+      end += 1;
+    }else break;
+  }
+  return end;
+}
+ /*
+  * A few assumptions: 
+  * It's ok to return 0 when we run out of string to process
+  * A full sexp is passed to this function (we don't run out of close-parens)
+  * return 0 on all errors
+  */
 struct sexp* sexp_parse(char* str){
+/*	if(str != 0){
+	  //first char should be '('
+	  if(*str == '('){
+	    struct sexp* new_sexp = malloc(sizeof(struct sexp));
+	    char* start = ltrim(str+1); //trim off the whiespace, if any
+	//FIXME: incomplete, very
+	  }else return 0;
+	}
+	else return 0; */
+//ignore this for the moment :)
 }
 
 /* assumes sexp_parse mallocs each sexp and string individually */
