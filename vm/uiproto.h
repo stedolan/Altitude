4e4c71155441989ab1045d90d719d3f6a6849e71
Mark King <marknking@hotmail.com>
2009-04-29 16:51:22 +0000

    uiproto.c tweaks
    git-svn-id: svn+ssh://spoon/var/www/vhosts/trac/altitude/svn/trunk@44 980be3b3-3046-4121-8ed5-61669e3ce2bc

--- vm/uiproto.h
+++ vm/uiproto.h
@@ -1,10 +1,6 @@
 #ifndef _ALTITUDE_UI_PROTO_H
 #define _ALTITUDE_UI_PROTO_H
-
-#ifndef _ALTITUDE_CONTROL_H
-#include "control.h"
-#endif
-
+#include "error.h"
 
 char ui_proto_command_buf[1024];//scratch space for command data
 struct location ui_last_location;//anytime a location is parsed, it goes in here
