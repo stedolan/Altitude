1c0c3dd544e17864d5f023247862d3d3e7404865
Mark King <marknking@hotmail.com>
2009-04-29 17:05:50 +0000

    uiproto.c
    git-svn-id: svn+ssh://spoon/var/www/vhosts/trac/altitude/svn/trunk@46 980be3b3-3046-4121-8ed5-61669e3ce2bc

--- vm/uiproto.c
+++ vm/uiproto.c
@@ -1,25 +1,19 @@
-#ifndef _ALTITUDE_UI_PROTO_H
 #include "uiproto.h"
-#endif
-#ifndef _ALTITUDE_ATOM_H
 #include "atom.h"
-#endif
-#ifndef _ALTITUDE_ERROR_H
 #include "error.h"
-#endif
 #include "stdio.h"
 #include "stdlib.h"
 
 void get_command(){
   int __a_raptor_is_you = scanf("%s", ui_proto_command_buf);
-  if(strncmp("set\0", ui_proto_command_buf, __a_raptor_is_you) == 0){
+  if(strncmp("set", ui_proto_command_buf, __a_raptor_is_you) == 0){
      int line_number=0;
      scanf("%d", &line_number);
      ui_last_location.filename = (current_location()).filename;
      ui_last_location.line = line_number;
      ui_last_location.bytepos = 0;
   }
-  if(strncmp("unset\0", ui_proto_command_buf, __a_raptor_is_you) == 0){
+  if(strncmp("unset", ui_proto_command_buf, __a_raptor_is_you) == 0){
      int line_number=0;
      scanf("%d", &line_number);
      ui_last_location.filename = (current_location()).filename;
@@ -29,19 +23,19 @@ void get_command(){
 }
 
 void parse_command(){
-  if(strncmp(ui_proto_command_buf, "run\0", 3) == 0){
-    altitude_run();
-  }else if(strncmp(ui_proto_command_buf, "runback\0", 7) == 0){
-    altitude_runback();
-  }else if(strncmp(ui_proto_command_buf, "restart\0", 7) == 0){
-    altitude_restart();
-  }else if(strncmp(ui_proto_command_buf, "view\0", 4) == 0){
-    altitude_view();
-  }else if(strncmp(ui_proto_command_buf, "set\0", 3) == 0){
-    breakpoint_set(&ui_last_location);
-  }else if(strncmp(ui_proto_command_buf, "unset\0", 5) == 0){
-    breakpoint_unset(&ui_last_location);
-  }else{
-    say(E_BADCOMMAND, "Unrecognised command");
-  }
+	if(get_atom("run") == get_atom(ui_proto_command_buf)) {
+		altitude_run();
+	} else if(get_atom("runback") == get_atom(ui_proto_command_buf)) {
+		altitude_runback();
+	}else if(get_atom("restart") == get_atom(ui_proto_command_buf)){
+		altitude_restart();
+	}else if(get_atom("view") == get_atom(ui_proto_command_buf)){
+		altitude_view();
+	}else if(get_atom("set") == get_atom(ui_proto_command_buf)){
+		breakpoint_set(&ui_last_location);
+	}else if(get_atom("unset") == get_atom(ui_proto_command_buf)){
+		breakpoint_unset(&ui_last_location);
+	}else{
+		say(E_BADCOMMAND, "Unrecognised command");
+	}
 }
