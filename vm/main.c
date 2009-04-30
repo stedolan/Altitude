ae7694874c051adfb047f07b7f0f07d06b6f393b
Stephen Dolan <mu@netsoc.tcd.ie>
2009-04-30 01:23:45 +0000

    added wrapper script
    git-svn-id: svn+ssh://spoon/var/www/vhosts/trac/altitude/svn/trunk@60 980be3b3-3046-4121-8ed5-61669e3ce2bc

--- vm/main.c
+++ vm/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "sexp.h"
 #include "bytecode.h"
@@ -8,23 +9,23 @@
 #define MAXFILE (1024*1024)
 
 int main(int argc, char** argv){
-  if (argc <= 1){
-    fprintf(stderr, "usage: %s <program>, where <program> is a sexp-compiled C file\n", argv[0]);
+  if (argc <= 2){
+    fprintf(stderr, "usage: %s [run|dump] <program>, where <program> is a sexp-compiled C file\n", argv[0]);
     exit(1);
   }
-  FILE* prog = fopen(argv[1], "r");
+  FILE* prog = fopen(argv[2], "r");
   if (!prog){
     perror("Couldn't open program");
   }
   char progbuf[MAXFILE];
   fread(progbuf, MAXFILE, 1, prog);
   if (!feof(prog)){
-    fprintf(stderr, "Couldn't read all of %s", argv[1]);
+    fprintf(stderr, "Couldn't read all of %s", argv[2]);
     exit(1);
   }
   struct sexp* sexpcode = sexp_parse(progbuf);
   if (!sexpcode){
-    fprintf(stderr, "Couldn't parse s-expressions in %s", argv[1]);
+    fprintf(stderr, "Couldn't parse s-expressions in %s", argv[2]);
     exit(1);
   }
   struct program* program = compile(sexpcode);
@@ -34,7 +35,10 @@ int main(int argc, char** argv){
     fprintf(stderr, "Couldn't compile\n");
     exit(1);
   }
-  program_dump(program);
-  printf("Program returned %d\n", run(program));
+  if (!strcmp(argv[1], "run")){
+    run(program);
+  }else if (!strcmp(argv[1],"dump")){
+    program_dump(program);
+  }
   return 0;
 }
